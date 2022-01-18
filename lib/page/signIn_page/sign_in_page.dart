de58a6d16574c0ecd49382b29966545b938581f2
haseebzahid1 <mmianhaseeb444@gmail.com>
2022-01-18 23:51:33 +0500

    FocusScope added

--- lib/page/signIn_page/sign_in_page.dart
+++ lib/page/signIn_page/sign_in_page.dart
@@ -45,141 +45,146 @@ class _LoginPageWidgetState extends State<LoginPageWidget> {
         backgroundColor: Colors.transparent,
         body: Padding(
           padding: EdgeInsets.fromLTRB(36, 0, 36, 0),
-          child: ListView(
-            children: [
-              Column(
-                children: [
-                  Column(
-                    children: [
-                      IconButton(onPressed:(){
-                        Navigator.of(context).pop();
-                      }, icon: Icon(Icons.arrow_back), iconSize: 44,),
-                      SizedBox(height: 10,),
-                      Image.asset("assets/images/logo.png",height: size.height * 0.10,),
-                    ],
-                  ),
-                  const SizedBox(height: 23,),
-                  Container(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+          child: GestureDetector(
+            onTap: (){
+              FocusScope.of(context).unfocus();
+            },
+            child: ListView(
+              children: [
+                Column(
+                  children: [
+                    Column(
                       children: [
-                        Text("Register with Socials",style: registerHeadingStyle ,),
-                        const SizedBox(height: 15,),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            SocialIcon(bgColor:bgButtonWhite, svgImage: 'assets/icons/google-icon.svg',onTap: (){ print("google");},),
-                            SocialIcon(bgColor:kbgBlue,color: Colors.white, svgImage: 'assets/icons/facebook-2.svg',onTap: (){print("facebook");},),
-                            SocialIcon(bgColor:lightBlue,color: Colors.white, svgImage: 'assets/icons/twitter.svg',onTap: (){print("twitter");},),
-                          ],
-                        )
+                        IconButton(onPressed:(){
+                          Navigator.of(context).pop();
+                        }, icon: Icon(Icons.arrow_back), iconSize: 44,),
+                        SizedBox(height: 10,),
+                        Image.asset("assets/images/logo.png",height: size.height * 0.10,),
                       ],
                     ),
-                  ),
-                  const SizedBox(height: 6,),
-                  DoubleLine(),
-                  const SizedBox(height: 6,),
-                  Row(
-                    children:  [
-                      Text("Login Using Email",style: registerHeadingStyle ,),
-                    ],
-                  ),
-                  const SizedBox(height: 15,),
-                  Form(
-                    key: _formKey,
-                    autovalidateMode: AutovalidateMode.always,
-                    child: Column(
-                      children: [
-                        InputFieldWidget(
-                          initialValue: signInProvider.user.email ?? "Email@gmail.com",
-                          // hintText: "Email@gmail.com",
-                          labelText: "Email",
-                          textInputType: TextInputType.emailAddress,
-                          validate:signInProvider.validateUsername,
-                          onSaved: signInProvider.onSaveUsername,
-                          prefixIcon: const Icon(Icons.person,color: lightBlue,),
-                          isPassword: false,
-                        ),
-                        const SizedBox(height: 17,),
-                        InputFieldWidget(
-                          initialValue: signInProvider.user.password ?? "12345678",
-                          labelText: "Password",
-                          prefixIcon: const Icon(Icons.https,color: lightBlue,),
-                          validate:signInProvider.validatePassword,
-                          onSaved: signInProvider.onSavedPassword,
-                          onChanged:signInProvider.onChancedPassword ,
-                        ),
-                        const SizedBox(height: 3,),
-                        Row(
-                          children: [
-                            Expanded(
+                    const SizedBox(height: 23,),
+                    Container(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text("Register with Socials",style: registerHeadingStyle ,),
+                          const SizedBox(height: 15,),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              SocialIcon(bgColor:bgButtonWhite, svgImage: 'assets/icons/google-icon.svg',onTap: (){ print("google");},),
+                              SocialIcon(bgColor:kbgBlue,color: Colors.white, svgImage: 'assets/icons/facebook-2.svg',onTap: (){print("facebook");},),
+                              SocialIcon(bgColor:lightBlue,color: Colors.white, svgImage: 'assets/icons/twitter.svg',onTap: (){print("twitter");},),
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                    const SizedBox(height: 6,),
+                    DoubleLine(),
+                    const SizedBox(height: 6,),
+                    Row(
+                      children:  [
+                        Text("Login Using Email",style: registerHeadingStyle ,),
+                      ],
+                    ),
+                    const SizedBox(height: 15,),
+                    Form(
+                      key: _formKey,
+                      autovalidateMode: AutovalidateMode.always,
+                      child: Column(
+                        children: [
+                          InputFieldWidget(
+                            initialValue: signInProvider.user.email ?? "Email@gmail.com",
+                            // hintText: "Email@gmail.com",
+                            labelText: "Email",
+                            textInputType: TextInputType.emailAddress,
+                            validate:signInProvider.validateUsername,
+                            onSaved: signInProvider.onSaveUsername,
+                            prefixIcon: const Icon(Icons.person,color: lightBlue,),
+                            isPassword: false,
+                          ),
+                          const SizedBox(height: 17,),
+                          InputFieldWidget(
+                            initialValue: signInProvider.user.password ?? "12345678",
+                            labelText: "Password",
+                            prefixIcon: const Icon(Icons.https,color: lightBlue,),
+                            validate:signInProvider.validatePassword,
+                            onSaved: signInProvider.onSavedPassword,
+                            onChanged:signInProvider.onChancedPassword ,
+                          ),
+                          const SizedBox(height: 3,),
+                          Row(
+                            children: [
+                              Expanded(
 
-                              flex:1,
-                              child: CheckboxListTile(
-                                contentPadding: EdgeInsets.all(0),
-                                  controlAffinity: ListTileControlAffinity.leading,
-                                title: Text("Remember me",style: forgetPassword),
-                                  value: signInProvider.remember,
-                                  onChanged: (bool? value) {signInProvider.checkBoxChecked(value);}
-                                  ),
-                            ),
-
-                            Expanded(
-                              // flex: 1,
-                              child: GestureDetector(
-                                onTap: (){
-                                    print("forget password");
-                                },
-                                child:  Text("Forgot Password",style: forgetPassword.copyWith(decoration: TextDecoration.underline, ),textAlign: TextAlign.right,),
+                                flex:1,
+                                child: CheckboxListTile(
+                                  contentPadding: EdgeInsets.all(0),
+                                    controlAffinity: ListTileControlAffinity.leading,
+                                  title: Text("Remember me",style: forgetPassword),
+                                    value: signInProvider.remember,
+                                    onChanged: (bool? value) {signInProvider.checkBoxChecked(value);}
+                                    ),
                               ),
-                            ),
-                          ],
+
+                              Expanded(
+                                // flex: 1,
+                                child: GestureDetector(
+                                  onTap: (){
+                                      print("forget password");
+                                  },
+                                  child:  Text("Forgot Password",style: forgetPassword.copyWith(decoration: TextDecoration.underline, ),textAlign: TextAlign.right,),
+                                ),
+                              ),
+                            ],
+                          ),
+
+
+                          FormButton(
+                            onTap: (){
+                              if(_formKey.currentState!.validate()){
+                                _formKey.currentState!.save();
+                                signInProvider.onSubmit();
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                    SnackBar(content: const Text("VaLIDATION PASSED"))
+                                );
+                              }else{
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                    const SnackBar(content: Text("VALIDATION ERROR")));
+                              }
+                              // Navigator.of(context).push(MaterialPageRoute(builder: (context)=>const AnyNamepage()));
+                            },
+                            padding:EdgeInsets.symmetric(vertical: 13) ,
+                            bgcolor: lightBlue.withOpacity(0.7), color: Colors.white,
+                            width: double.infinity, textButton: 'Log in',),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(height: 50,),
+                    // Spacer(),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text("Not a member?",
+                          style: memberHeadingStyle,
                         ),
-
-
-                        FormButton(
+                        SizedBox(width: 3,),
+                        InkWell(
+                          // onTap: onTab,
                           onTap: (){
-                            if(_formKey.currentState!.validate()){
-                              _formKey.currentState!.save();
-                              signInProvider.onSubmit();
-                              ScaffoldMessenger.of(context).showSnackBar(
-                                  SnackBar(content: const Text("VaLIDATION PASSED"))
-                              );
-                            }else{
-                              ScaffoldMessenger.of(context).showSnackBar(
-                                  const SnackBar(content: Text("VALIDATION ERROR")));
-                            }
-                            // Navigator.of(context).push(MaterialPageRoute(builder: (context)=>const AnyNamepage()));
+                            Navigator.of(context).push(MaterialPageRoute(builder: (context)=> SignUpPage()));
                           },
-                          padding:EdgeInsets.symmetric(vertical: 13) ,
-                          bgcolor: lightBlue.withOpacity(0.7), color: Colors.white,
-                          width: double.infinity, textButton: 'Log in',),
+                          child:  Text("Sign up here",
+                            style: memberHeadingStyle.copyWith(color: Colors.blue),
+                          ),
+                        ),
                       ],
                     ),
-                  ),
-                  const SizedBox(height: 50,),
-                  // Spacer(),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text("Not a member?",
-                        style: memberHeadingStyle,
-                      ),
-                      SizedBox(width: 3,),
-                      InkWell(
-                        // onTap: onTab,
-                        onTap: (){
-                          Navigator.of(context).push(MaterialPageRoute(builder: (context)=> SignUpPage()));
-                        },
-                        child:  Text("Sign up here",
-                          style: memberHeadingStyle.copyWith(color: Colors.blue),
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ],
+                  ],
+                ),
+              ],
+            ),
           ),
         )
       ),
--- lib/page/signUp_page/signUp_page.dart
+++ lib/page/signUp_page/signUp_page.dart
@@ -45,134 +45,139 @@ class HomePageWidget extends StatelessWidget {
         backgroundColor: Colors.transparent,
         body: Padding(
           padding: EdgeInsets.fromLTRB(36, 0, 36, 0),
-          child: ListView(
-            children: [
-              Column(
-                children: [
-                  Column(
-                    children: [
-                      IconButton(onPressed:(){
-                        Navigator.of(context).pop();
-                      }, icon: Icon(Icons.arrow_back), iconSize: 44,),
-                      // SizedBox(height: 10,),
-                      Image.asset("assets/images/logo.png",height: size.height * 0.10,),
-                    ],
-                  ),
-                  const SizedBox(height: 18,),
-                  Container(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+          child: GestureDetector(
+            onTap: (){
+             FocusScope.of(context).unfocus();
+            },
+            child: ListView(
+              children: [
+                Column(
+                  children: [
+                    Column(
                       children: [
-                        Text("Register with socials",style: registerHeadingStyle ,),
-                        const SizedBox(height: 10,),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            SocialIcon(bgColor:bgButtonWhite, svgImage: 'assets/icons/google-icon.svg',onTap: (){ print("google");},),
-                            SocialIcon(bgColor:kbgBlue,color: Colors.white, svgImage: 'assets/icons/facebook-2.svg',onTap: (){print("facebook");},),
-                            SocialIcon(bgColor:lightBlue,color: Colors.white, svgImage: 'assets/icons/twitter.svg',onTap: (){print("twitter");},),
-                          ],
-                        )
+                        IconButton(onPressed:(){
+                          Navigator.of(context).pop();
+                        }, icon: Icon(Icons.arrow_back), iconSize: 44,),
+                        // SizedBox(height: 10,),
+                        Image.asset("assets/images/logo.png",height: size.height * 0.10,),
                       ],
                     ),
-                  ),
-                  // const SizedBox(height: 6,),
-                  DoubleLine(),
-                  // const SizedBox(height: 6,),
-                  Row(
-                    children:  [
-                      Text("Register with email",style:  registerHeadingStyle,),
-                    ],
-                  ),
-                  const SizedBox(height: 12,),
-                  Form(
-                    key: _formKey,
-                    autovalidateMode: AutovalidateMode.always,
-                    child: Column(
-                      children: [
-                        InputFieldWidget(
-                          initialValue: signUpProvider.signUpUser.email ?? "Email@gmail.com",
-                          hintText: "Email@gmail.com",
-                          labelText: "Email",
-                          textInputType:TextInputType.emailAddress,
-                          validate:signUpProvider.validateUsername,
-                          onSaved: signUpProvider.onSaveUsername,
-                          prefixIcon: const Icon(Icons.person,color: lightBlue,),
-                          isPassword: false,
-                        ),
-                        const SizedBox(height: 18,),
-                        InputFieldWidget(
-                          initialValue: signUpProvider.signUpUser.password ?? "12345678",
-                          labelText: "Password",
-                          prefixIcon: const Icon(Icons.https,color: lightBlue,),
-                          validate:signUpProvider.validatePassword,
-                          onSaved: signUpProvider.onSavedPassword,
-                          onChanged:signUpProvider.onChancedPassword ,
-                        ),const SizedBox(height: 15,),
-                        InputFieldWidget(
-                          initialValue: "12345678",
-                          labelText: "Confirm Password",
-                          prefixIcon: const Icon(Icons.https,color: lightBlue,),
-                          validate:signUpProvider.validateConfirmPassword,
-                          onChanged: signUpProvider.onChangedConfirmPassword,
-                          onSaved:signUpProvider.onSaveConfirmPassword,
-                        ),
-                        const SizedBox(height: 7,),
-                        Padding(
-                          padding: const EdgeInsets.only(right: 13),
-                          child: RichText(
-                            text: TextSpan(
-                              children: [
-                                TextSpan(text: 'Bt creating an account I accept the ',style: privacyHeadingStyle),
-                                TextSpan(text: 'Terms & Conditions', style: privacyHeadingStyle.copyWith(color: Colors.blue),recognizer: gestureTermsCondition),
-                                TextSpan(text: 'and',style: privacyHeadingStyle),
-                                TextSpan(text: 'privacy policy',style: privacyHeadingStyle.copyWith(color: Colors.blue),recognizer: gesturePrivacyPolicy),
-                              ],
+                    const SizedBox(height: 18,),
+                    Container(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text("Register with socials",style: registerHeadingStyle ,),
+                          const SizedBox(height: 10,),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              SocialIcon(bgColor:bgButtonWhite, svgImage: 'assets/icons/google-icon.svg',onTap: (){ print("google");},),
+                              SocialIcon(bgColor:kbgBlue,color: Colors.white, svgImage: 'assets/icons/facebook-2.svg',onTap: (){print("facebook");},),
+                              SocialIcon(bgColor:lightBlue,color: Colors.white, svgImage: 'assets/icons/twitter.svg',onTap: (){print("twitter");},),
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                    // const SizedBox(height: 6,),
+                    DoubleLine(),
+                    // const SizedBox(height: 6,),
+                    Row(
+                      children:  [
+                        Text("Register with email",style:  registerHeadingStyle,),
+                      ],
+                    ),
+                    const SizedBox(height: 12,),
+                    Form(
+                      key: _formKey,
+                      autovalidateMode: AutovalidateMode.always,
+                      child: Column(
+                        children: [
+                          InputFieldWidget(
+                            initialValue: signUpProvider.signUpUser.email ?? "Email@gmail.com",
+                            hintText: "Email@gmail.com",
+                            labelText: "Email",
+                            textInputType:TextInputType.emailAddress,
+                            validate:signUpProvider.validateUsername,
+                            onSaved: signUpProvider.onSaveUsername,
+                            prefixIcon: const Icon(Icons.person,color: lightBlue,),
+                            isPassword: false,
+                          ),
+                          const SizedBox(height: 18,),
+                          InputFieldWidget(
+                            initialValue: signUpProvider.signUpUser.password ?? "12345678",
+                            labelText: "Password",
+                            prefixIcon: const Icon(Icons.https,color: lightBlue,),
+                            validate:signUpProvider.validatePassword,
+                            onSaved: signUpProvider.onSavedPassword,
+                            onChanged:signUpProvider.onChancedPassword ,
+                          ),const SizedBox(height: 15,),
+                          InputFieldWidget(
+                            initialValue: "12345678",
+                            labelText: "Confirm Password",
+                            prefixIcon: const Icon(Icons.https,color: lightBlue,),
+                            validate:signUpProvider.validateConfirmPassword,
+                            onChanged: signUpProvider.onChangedConfirmPassword,
+                            onSaved:signUpProvider.onSaveConfirmPassword,
+                          ),
+                          const SizedBox(height: 7,),
+                          Padding(
+                            padding: const EdgeInsets.only(right: 13),
+                            child: RichText(
+                              text: TextSpan(
+                                children: [
+                                  TextSpan(text: 'Bt creating an account I accept the ',style: privacyHeadingStyle),
+                                  TextSpan(text: 'Terms & Conditions', style: privacyHeadingStyle.copyWith(color: Colors.blue),recognizer: gestureTermsCondition),
+                                  TextSpan(text: 'and',style: privacyHeadingStyle),
+                                  TextSpan(text: 'privacy policy',style: privacyHeadingStyle.copyWith(color: Colors.blue),recognizer: gesturePrivacyPolicy),
+                                ],
+                              ),
                             ),
                           ),
+                          const SizedBox(height: 20,),
+                          FormButton(
+                            onTap: (){
+                              if(_formKey.currentState!.validate()){
+                                _formKey.currentState!.save();
+                                signUpProvider.onSubmit();
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                    SnackBar(content: Text("VaLIDATION PASSED"))
+                                );
+                              }else{
+                                signUpProvider.onSubmit();
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                    SnackBar(content: Text("VALIDATION ERROR")));
+                              }
+                              Navigator.of(context).push(MaterialPageRoute(builder: (context)=>LoginPage()));
+                            },
+                            padding:EdgeInsets.symmetric(vertical: 13) ,
+                            bgcolor: lightBlue, color: Colors.white,
+                            width: double.infinity, textButton: 'Join Us',),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(height: 10,),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text("Already a member?",
+                          style: memberHeadingStyle,
                         ),
-                        const SizedBox(height: 20,),
-                        FormButton(
-                          onTap: (){
-                            if(_formKey.currentState!.validate()){
-                              _formKey.currentState!.save();
-                              signUpProvider.onSubmit();
-                              ScaffoldMessenger.of(context).showSnackBar(
-                                  SnackBar(content: Text("VaLIDATION PASSED"))
-                              );
-                            }else{
-                              signUpProvider.onSubmit();
-                              ScaffoldMessenger.of(context).showSnackBar(
-                                  SnackBar(content: Text("VALIDATION ERROR")));
-                            }
-                            Navigator.of(context).push(MaterialPageRoute(builder: (context)=>LoginPage()));
-                          },
-                          padding:EdgeInsets.symmetric(vertical: 13) ,
-                          bgcolor: lightBlue, color: Colors.white,
-                          width: double.infinity, textButton: 'Join Us',),
+                        const SizedBox(width: 3,),
+                        InkWell(
+                          // onTap: onTab,
+                          onTap: (){},
+                          child:  Text("Sign in",
+                            style: memberHeadingStyle.copyWith(color: Colors.blue),
+                          ),
+                        ),
                       ],
                     ),
-                  ),
-                  const SizedBox(height: 10,),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text("Already a member?",
-                        style: memberHeadingStyle,
-                      ),
-                      const SizedBox(width: 3,),
-                      InkWell(
-                        // onTap: onTab,
-                        onTap: (){},
-                        child:  Text("Sign in",
-                          style: memberHeadingStyle.copyWith(color: Colors.blue),
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ],
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       )
